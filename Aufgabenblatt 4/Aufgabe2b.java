a97adc468736afb0e674588a23d6de6d8064f1f7
EricZeller <98104805+EricZeller@users.noreply.github.com>
2025-04-09 10:37:01 +0200

    blatt4

--- Aufgabenblatt 4/Aufgabe2b.java
+++ Aufgabenblatt 4/Aufgabe2b.java
@@ -24,7 +24,7 @@ public class Aufgabe2b {
     // gibt die Wurzel zurück, falls diese ganzzahlig ist. Sonst -1
 
     public static void main(String[] args) {
-        System.out.println(fBinary(Integer.parseInt(System.console().readLine())));
+        System.out.println(fBinary(Integer.parseInt(System.console().readLine("Bitte Zahl eingeben von der die Wurzel gezogen werden soll: "))));
     }
 }
 
