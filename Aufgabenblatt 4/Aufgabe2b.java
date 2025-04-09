256bef4cb1a02cc04a578edf850148cf0ee600b2
EricZeller <98104805+EricZeller@users.noreply.github.com>
2025-04-09 10:32:37 +0200

    aufgabenblatt4

---
+++ Aufgabenblatt 4/Aufgabe2b.java
@@ -0,0 +1,35 @@
+public class Aufgabe2b {
+    public static int fBinary(int number) {
+        if (number < 0)
+            return -1; 
+
+        int left = 1, right = number;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            long square = (long) mid * mid; 
+
+            if (square == number) {
+                return mid;
+            } else if (square < number) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return -1;
+    }
+
+    
+    // gibt die Wurzel zurück, falls diese ganzzahlig ist. Sonst -1
+
+    public static void main(String[] args) {
+        System.out.println(fBinary(Integer.parseInt(System.console().readLine())));
+    }
+}
+
+/*
+ * TBest(n) = O(1)
+ * TWorst(n) = O(log(n))
+ * TAvg(n) = O(log(n)/2) = O(log(n))
+ */
