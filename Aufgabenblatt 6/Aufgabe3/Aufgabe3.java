889d9e578549f15ebbc41d9f28e50be4100fdd0d
EricZeller <98104805+EricZeller@users.noreply.github.com>
2025-05-03 10:36:45 +0200

    blatt 6 aufgabe 3b fix

--- Aufgabenblatt 6/Aufgabe3/Aufgabe3.java
+++ Aufgabenblatt 6/Aufgabe3/Aufgabe3.java
@@ -3,6 +3,7 @@ package Aufgabe3;
 public class Aufgabe3 {
     public static void main(String[] args) {
         // 3a
+        System.out.println("Aufgabe 3a:");
         Currency eur = new Euro();
         Currency usd = new USD();
         Currency sfr = new SFR();
@@ -10,6 +11,14 @@ public class Aufgabe3 {
         System.out.println(eur.convert(100.0, sfr) + " " + sfr.getName());
         System.out.println(eur.convert(100.0, usd) + " " + usd.getName());
         System.out.println(usd.convert(100, eur) + " " + eur.getName());
+        System.out.println();
+
+        // 3b
+        System.out.println("Aufgabe 3b:");
+        Bill bill = new Bill(100.0, usd);
+        bill.dumpAmount(eur);
+        bill.dumpAmount(sfr);
+        bill.dumpAmount(usd);
 
         /*
          * Gewünschte Ausgabe:
@@ -18,6 +27,11 @@ public class Aufgabe3 {
          * 93.45794392523364 SFR
          * 114.94252873563218 USD
          * 87.0 EUR
+         * 
+         * 3b:
+         * 87.0 EUR
+         * 81.30841121495327 SFR
+         * 100.0 USD
          */
     }
 }
